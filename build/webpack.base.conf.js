600a985b9e07bd4cfd870a54e142ff672aebf1a6
Alfred Li <Alfred.Li@ringcentral.com>
2024-06-27 11:10:37 +0800

    feat: update webpack plugin

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,7 +1,15 @@
+const mode = process.env.MODE || 'local';
+const { version } = require('../package.json');
+
+const { DefinePlugin } = require('webpack');
 const paths = require('./utils/paths');
 const { VueLoaderPlugin } = require('vue-loader');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const HTMLPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const config = require('./config');
+const { env } = require(`../config/${mode}.env.js`);
+const dayjs = require('dayjs');
 
 const isProd = process.env.NODE_ENV === 'production';
 const outputFileName = `js/[name]${isProd ? '.[contenthash:8]' : ''}.js`;
@@ -28,6 +36,7 @@ module.exports = {
   },
 
   module: {
+    noParse: /^(vue|vue-router|pinia)$/,
     rules: [
       {
         test: /\.vue$/,
@@ -45,6 +54,50 @@ module.exports = {
         },
         use: ['thread-loader', 'babel-loader'],
       },
+      {
+        test: /\.tsx?$/,
+        use: [
+          'thread-loader',
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              appendTsSuffixTo: ['\\.vue$'],
+              happyPackMode: true,
+            },
+          },
+        ],
+      },
+
+      // images
+      {
+        test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+        type: 'asset',
+        generator: { filename: 'img/[contenthash:8][ext][query]' },
+      },
+
+      // do not base64-inline SVGs.
+      // https://github.com/facebookincubator/create-react-app/pull/1180
+      {
+        test: /\.(svg)(\?.*)?$/,
+        type: 'asset/resource',
+        generator: { filename: 'img/[contenthash:8][ext][query]' },
+      },
+
+      // media
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        type: 'asset',
+        generator: { filename: 'media/[contenthash:8][ext][query]' },
+      },
+
+      // fonts
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        type: 'asset',
+        generator: { filename: 'fonts/[contenthash:8][ext][query]' },
+      },
     ],
   },
 
@@ -54,6 +107,29 @@ module.exports = {
       template: paths.resolve('public/index.html'),
       templateParameters: {
         BASE_URL: '/',
+      },
+    }),
+    new CaseSensitivePathsPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: paths.resolve('public'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['.DS_Store', '**/index.html'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+    new DefinePlugin({
+      // vue3 feature flags <http://link.vuejs.org/feature-flags>
+      __VUE_OPTIONS_API__: 'true',
+      __VUE_PROD_DEVTOOLS__: 'false',
+      'process.env': {
+        ...env,
+        VERSION: JSON.stringify(version),
+        BUILD_TIME: JSON.stringify(dayjs().format('YYYY-MM-DD HH:mm:ss')),
       },
     }),
   ],
