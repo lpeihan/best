921b046cc8489903bd868788742c4d106abdebfd
Alfred Li <Alfred.Li@ringcentral.com>
2024-06-28 10:21:05 +0800

    feat: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    // 'stylelint',
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
